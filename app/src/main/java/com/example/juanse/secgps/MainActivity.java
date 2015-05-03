67c11f874745cae6be0f0771e81bfb674a396853
juansecaro <juanseb@gmail.com>
2015-05-03 04:00:19 +0200

    Funciona con ArrayList Adapter y GPS comentado Implementando Main Activity con buscador

--- app/src/main/java/com/example/juanse/secgps/MainActivity.java
+++ app/src/main/java/com/example/juanse/secgps/MainActivity.java
@@ -23,33 +23,27 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ListView list = new ListView(this);
-        setContentView(list);
-        CustomAdapter adapter = new CustomAdapter(this,
-                R.layout.custom_row,
-                R.id.line1,
-                new String[] {"Bill","Tom","Sally","Jenny"});
+        setContentView(R.layout.activity_main);//list
+        CustomAdapter adapter = new CustomAdapter(this,R.layout.custom_row,R.id.line1,new String[] {"Nuevo","Continuar","Logros","Configuración","Novedades"});
         list.setAdapter(adapter);
     }
+
     private static class CustomAdapter extends ArrayAdapter<String> {
-        public CustomAdapter(Context context, int layout, int resId,
-                             String[] items) {
-//Call through to ArrayAdapter implementation
+        public CustomAdapter(Context context, int layout, int resId,String[] items) {
+        //Call through to ArrayAdapter implementation
             super(context, layout, resId, items);
         }
         @Override
-        public View getView(int position, View convertView,
-                            ViewGroup parent) {
+        public View getView(int position, View convertView,ViewGroup parent) {
+
             View row = convertView;
-//Inflate a new row if one isn't recycled
+            //Inflate a new row if one isn't recycled
             if(row == null) {
-                row = LayoutInflater.from(getContext())
-                        .inflate(R.layout.custom_row, parent, false);
+                row = LayoutInflater.from(getContext()).inflate(R.layout.custom_row, parent, false);
             }
             String item = getItem(position);
-            ImageView left =
-                    (ImageView)row.findViewById(R.id.leftimage);
-            ImageView right =
-                    (ImageView)row.findViewById(R.id.rightimage);
+            ImageView left = (ImageView)row.findViewById(R.id.leftimage);
+            ImageView right = (ImageView)row.findViewById(R.id.rightimage);
             TextView text = (TextView)row.findViewById(R.id.line1);
             left.setImageResource(R.drawable.left_arrow);
             right.setImageResource(R.drawable.right_arrow);
