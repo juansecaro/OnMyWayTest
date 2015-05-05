f8b916bce472aa82eefc37e8243c8f419f654af2
juansecaro <juanseb@gmail.com>
2015-05-05 18:05:35 +0200

    novedades casi

--- app/src/main/java/com/example/juanse/secgps/MainActivity.java
+++ app/src/main/java/com/example/juanse/secgps/MainActivity.java
@@ -1,30 +1,51 @@
 package com.example.juanse.secgps;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 
 //---------------------
 
 
-
 public class MainActivity extends ListActivity {
-
 
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ListView list = new ListView(this);
         setContentView(R.layout.activity_main);//list
-       // CustomAdapter adapter = new CustomAdapter(this,R.layout.custom_row,R.id.line1,
-         //       new String[] {"Nuevo","Continuar","Logros","Configuración","Novedades"});
+        // CustomAdapter adapter = new CustomAdapter(this,R.layout.custom_row,R.id.line1,
+        //       new String[] {"Nuevo","Continuar","Logros","Configuración","Novedades"});
         //ListView listView = (ListView) findViewById(R.id.lista);
 
-       // list.setAdapter(adapter);
+        // list.setAdapter(adapter);
         String[] adobe_products = getResources().getStringArray(R.array.adobe_products);
         this.setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, R.id.label, adobe_products));
+
+        ListView lv = getListView();
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                //select item
+                String selection = ((TextView) v).getText().toString();
+                // Launching new Activity on selecting single List Item
+                if (selection.equals("Novedades")) {
+                    Intent i = new Intent(getApplicationContext(), Novedades.class);
+                    // sending data to new activity
+                    //i.putExtra("product", product);
+                    startActivity(i);
+                }
+
+
+            }
+        });
     }
 
 
---
+++ app/src/main/java/com/example/juanse/secgps/Novedades.java
@@ -0,0 +1,56 @@
+package com.example.juanse.secgps;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by Juanse on 05/05/2015.
+ */
+public class Novedades extends Activity {
+
+    TextView textMsg, textPrompt;
+    final String textSource = "http://www.adeter.org/omw/novedades_es.txt";
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.novedades);
+        textPrompt = (TextView)findViewById(R.id.textprompt);
+        textMsg = (TextView)findViewById(R.id.textmsg);
+
+        textPrompt.setText("Wait...");
+
+        URL textUrl;
+        try {
+            textUrl = new URL(textSource);
+            BufferedReader bufferReader = new BufferedReader(new InputStreamReader(textUrl.openStream()));
+            String StringBuffer;
+            String stringText = "";
+            while ((StringBuffer = bufferReader.readLine()) != null) {
+                stringText += StringBuffer;
+            }
+            bufferReader.close();
+            textMsg.setText(stringText);
+        } catch (MalformedURLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            textMsg.setText(e.toString());
+        } catch (IOException e) {
+            /* TODO Auto-generated catch block */
+            e.printStackTrace();
+            textMsg.setText(e.toString());
+        }
+
+        textPrompt.setText("Finished!");
+
+    }
+
+}
