2d0c249f2147f30e0870fe0053550eb0e200bfcc
juansecaro <juanseb@gmail.com>
2015-05-08 03:49:21 +0200

    terminando novedades Async

--- app/src/main/java/com/example/juanse/secgps/Novedades.java
+++ app/src/main/java/com/example/juanse/secgps/Novedades.java
@@ -1,7 +1,9 @@
 package com.example.juanse.secgps;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
@@ -17,41 +19,67 @@ import java.net.URL;
 public class Novedades extends Activity {
 
     TextView textMsg, textPrompt;
+
     final String textSource = "http://www.adeter.org/omw/novedades_es.txt";
+    private ProgressBar mProgress;
 
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.novedades);
+
+        String news="";
+        mProgress = (ProgressBar)findViewById(R.id.mProgressBar);
         textPrompt = (TextView)findViewById(R.id.textprompt);
         textMsg = (TextView)findViewById(R.id.textmsg);
 
-        textPrompt.setText("Wait...");
+        TextFromURL TUrl = new TextFromURL();
+        TUrl.execute(news);
 
-        URL textUrl;
-        try {
-            textUrl = new URL(textSource);
-            BufferedReader bufferReader = new BufferedReader(new InputStreamReader(textUrl.openStream()));
-            String StringBuffer;
-            String stringText = "";
-            while ((StringBuffer = bufferReader.readLine()) != null) {
-                stringText += StringBuffer;
-            }
-            bufferReader.close();
-            textMsg.setText(stringText);
-        } catch (MalformedURLException e) {
 
-            e.printStackTrace();
-            textMsg.setText(e.toString());
-        } catch (IOException e) {
-
-            e.printStackTrace();
-            textMsg.setText(e.toString());
-        }
-
-        textPrompt.setText("Finished!");
+        textPrompt.setText(news);
 
     }
 
+    //---
+    public class TextFromURL extends AsyncTask<String,Integer,String>
+    {
+        @Override
+        protected void onPreExecute(){
+        }
+        @Override
+        protected String doInBackground(String...params){
+
+            String result = "";
+            try {
+                URL url = new URL("http://www.adeter.org/omw/novedades_es.txt");
+
+                BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+                String line = null;
+
+                while ((line = in.readLine()) != null) {
+                    //get lines
+                    result+=line;
+                }
+                in.close();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return result;
+        }
+        @Override
+        protected void onProgressUpdate(Integer...values){
+
+            mProgress.setProgress(values[0]);
+        }
+       /* @Override
+        protected void onPostExecute(){
+
+
+        }*/
+    }
 }
--- app/src/main/java/com/example/juanse/secgps/TextFromURL.java
+++
@@ -1,7 +0,0 @@
-package com.example.juanse.secgps;
-
-/**
- * Created by Juanse on 07/05/2015.
- */
-public class TextFromURL {
-}
