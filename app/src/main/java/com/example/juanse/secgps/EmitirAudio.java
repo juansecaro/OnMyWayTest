7bf36f1dc7cc7cf23276b7b5bcfe570529871a65
juansecaro <juanseb@gmail.com>
2015-05-11 03:35:43 +0200

    Separacion clases audio y gps async

---
+++ app/src/main/java/com/example/juanse/secgps/EmitirAudio.java
@@ -0,0 +1,14 @@
+package com.example.juanse.secgps;
+
+import android.os.AsyncTask;
+
+import java.net.URL;
+
+/**
+ * Created by Juanse on 11/05/2015.
+ */
+public class EmitirAudio extends AsyncTask<String, Integer, Long> {
+
+
+
+}
